7662ff77adde708819335f76f57ed21da4bff14f
Gianluca Paz <gianlucapaz@gmail.com>
2026-06-03 04:34:33 -0300

    chore: bump versão para 2.0.0 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "br.recycleapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
